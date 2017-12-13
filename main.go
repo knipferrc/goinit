475495143f5d1693f243dddec2af23ef67a4922d
knipferrc <knipferrc@gmail.com>
2017-12-13 08:23:17 -0500

    fix path

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "os"
 func main() {
 	if len(os.Args) > 1 {
 		if os.Args[1] == "react-app" {
-			CopyDir("./projects/react-app", "./react-app")
+			CopyDir("./trunks/react-app", "./react-app")
 		}
 		if os.Args[1] == "react-component-lib" {
 			CopyDir("./trunks/react-component-lib", "./react-component-lib")
